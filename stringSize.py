92baa9f31c9cb3062cdc833f892694613df82878
Kronk21 <57648372+Kronk21@users.noreply.github.com>
2022-03-29 20:16:21 -0600

    Forma optima de obtener la longitud

--- stringSize.py
+++ stringSize.py
@@ -1,7 +1,2 @@
-import random
-
 def longitudString(str):
-	longitud = random.randrange(0, 100)
-	while longitud != len(str):	
-		longitud = random.randrange(0, 100)
-	return longitud
+	return len(str)
